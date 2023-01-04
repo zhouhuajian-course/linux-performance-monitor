5469cba8a0bc588e0c400dd09f66a1600220ee0b
zhouhuajian <zhouhuajian@company.com>
2023-01-04 18:15:26 +0800

    Add a simple web server

---
+++ simple-web-server/src/main/java/simple/web/server/pojo/Response.java
@@ -0,0 +1,53 @@
+package simple.web.server.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Response {
+
+    private int code;
+    private String message;
+    private Object data;
+
+    private Response() {
+        this(0, "", null);
+    }
+
+    private Response(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static Response success(String message, Object data) {
+        return new Response(0, message, data);
+    }
+
+//    public static Response error(int code, String message) {
+//        return new Response(code, message, null);
+//    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
